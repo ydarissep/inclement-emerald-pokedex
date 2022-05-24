70b194216d13d889b5e3a856289bf483de387c23
kris <kmsedu@tuta.io>
2022-05-25 07:24:38 +1000

    Set up style-loader & css-loader for Live Reload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles/style.css'
 import buildPokemonObj from './helpers/buildPokemonObj'
 
 buildPokemonObj()
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
