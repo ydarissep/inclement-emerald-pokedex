17d97eb22cb33bcf8c86293815307de5383192a8
ydarissep <60859033+ydarissep@users.noreply.github.com>
2022-05-28 01:32:48 +0200

    Delete src directory

--- src/api/getSpecies.js
+++
@@ -1,8 +0,0 @@
-async function getSpecies () {
-  const rawSpecies = await fetch('https://raw.githubusercontent.com/BuffelSaft/pokeemerald/master/include/constants/species.h')
-  const textSpecies = await rawSpecies.text()
-
-  return textSpecies
-}
-
-export default getSpecies
