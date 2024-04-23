c14af3ee4c11cf51622f9ac7ce91c0ff757097c9
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-04-23 04:04:46 +0200

    Delete src/trainers directory

--- src/trainers/displayTrainers.js
+++
@@ -1,7 +0,0 @@
-fetch("https://raw.githubusercontent.com/ydarissep/dex-core/main/src/trainers/displayTrainers.js").then(response => {
-    return response.text()
-}).then(text => {
-    eval.call(window,text)
-}).catch(error => {
-    console.warn(error)
-})
